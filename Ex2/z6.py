d39fa4210ab839c4ca831cbbdf397915e63a925b
Stipo Margić <stipo@liberato.io>
2022-03-27 10:07:52 +0200

    Update z6.py
    z6 solved

--- Ex2/z6.py
+++ Ex2/z6.py
@@ -1,4 +1,11 @@
-  def check_is_result_zero(lst):
-  if 
+def check_is_result_zero(lst, num):
+  if not lst:
+    if num == 0:
+      return True
+    return False
 
-print(check_is_result_zero([1,2,3]))
+  if check_is_result_zero(lst[1:], lst[0] + num) or check_is_result_zero(lst[1:], lst[0] - num):
+    return True
+  return False
+
+print(check_is_result_zero([1,4, 5,2,4], 0))
